2c8c671a7e6940cfc20069936b0c86e3024bbff8
guoshikang <guoshikang@machloop.com>
2023-02-01 10:27:10 +0800

    feat：flink test

---
+++ flink-job/src/main/java/com/scorpio/Main.java
@@ -0,0 +1,13 @@
+package com.scorpio;
+
+/**
+* 
+* @author ${USER}
+*
+* create at ${DATE}, ${PROJECT_NAME}
+*/
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
